a75ee08bfcfb183790632a17ba19e3c449a843bb
Floom <floomait@gmail.com>
2024-02-05 15:03:47 +0200

    Fixed OpenAI image generation

--- Floom.Plugins/Model/Connectors/OpenAi/OpenAiModelConnectorPlugin.cs
+++ Floom.Plugins/Model/Connectors/OpenAi/OpenAiModelConnectorPlugin.cs
@@ -1,5 +1,7 @@
 using Floom.Model;
+using Floom.Pipeline;
 using Floom.Plugin.Base;
+using Microsoft.Extensions.Logging;
 
 namespace Floom.Plugins.Model.Connectors.OpenAi;
 
@@ -9,5 +11,32 @@ public class OpenAiModelConnectorPlugin : ModelConnectorPluginBase<OpenAiClient>
     protected override void InitializeClient(ModelConnectorPluginConfig settings)
     {
         _client.ApiKey = settings.ApiKey;
+    }
+    
+    protected override FloomPromptResponse ProcessPromptRequest(PipelineContext pipelineContext, FloomPromptRequest promptRequest)
+    {
+        var responseConfig = pipelineContext.Pipeline.Response?.Format?.First();
+
+        var responseType = "text";
+        
+        if (responseConfig != null)
+        {
+            var responseTypeConfig = responseConfig.Configuration.GetValueOrDefault("type", "text");
+            responseType = responseTypeConfig.ToString();
+        }
+        
+        if (responseType == "text")
+        {
+            return _client.GenerateTextAsync(promptRequest, _settings.Model).Result;
+        }
+
+        if(responseType == "image")
+        {
+            return _client.GenerateImageAsync(promptRequest, _settings.Model).Result;
+        }
+        
+        _logger.LogError($"Invalid response type: {responseType}");
+        
+        return new FloomPromptResponse();
     }
 }
