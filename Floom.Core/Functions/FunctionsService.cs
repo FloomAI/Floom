7a04916802caa8824a5d50ddf809f4c407730b14
Floom <floomait@gmail.com>
2024-09-30 20:24:19 +0300

    Update FunctionsService.cs

--- Floom.Core/Functions/FunctionsService.cs
+++ Floom.Core/Functions/FunctionsService.cs
@@ -293,6 +293,9 @@ public class FunctionsService : IFunctionsService
             Console.WriteLine("Function: " + function.Id + ", name: " + function.name + ", userId: " + function.userId);
             var user = await _userRepository.Get(function.userId, "_id");
             if (user == null)
+            {
+                continue;
+            }
             Console.WriteLine("User: " + user.Id + ", username: " + user.username + ", nickname: " + user.nickname);
             var authorName = !string.IsNullOrEmpty(user.nickname) ? user.nickname : user.username;
 
